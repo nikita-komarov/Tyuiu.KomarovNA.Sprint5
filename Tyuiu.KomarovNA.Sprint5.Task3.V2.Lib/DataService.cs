1795ffa4f53929b0ee0948485e826dc632922b98
gravity34031 <gravity2507@gmail.com>
2024-10-23 17:09:54 +0500

    task3

---
+++ Tyuiu.KomarovNA.Sprint5.Task3.V2.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KomarovNA.Sprint5.Task3.V2.Lib
+{
+    public class DataService : ISprint5Task3V2
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask3.bin");
+            double res = Math.Round(Math.Pow(Math.E, x) / x, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+            {
+                writer.Write(res);
+            }
+
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint5.Task3.V2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KomarovNA.Sprint5.Task3.V2.Lib;
+namespace Tyuiu.KomarovNA.Sprint5.Task3.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #2                                                            *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.SaveToFileTextData(3);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
