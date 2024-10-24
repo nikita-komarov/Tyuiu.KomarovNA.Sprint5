0281bf6417286f54b471f0a34a13f4a93053fbb8
gravity34031 <gravity2507@gmail.com>
2024-10-25 01:23:53 +0500

    Task5

---
+++ Tyuiu.KomarovNA.Sprint5.Task5.V17.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Tyuiu.KomarovNA.Sprint5.Task5.V17.Lib
+{
+    public class DataService : ISprint5Task5V17
+    {
+        public double LoadFromDataFile(string path)
+        {
+            int[] arr = File.ReadAllText(path).Split().Select(el => int.Parse(el)).ToArray();
+            double res = 0;
+            bool is_prime = true;
+            foreach (int num in arr)
+            {
+                is_prime = true;
+                for (int i = 2; i <= (int) (num / 2); i++)
+                {
+                    //Console.Write($"num={num}, i={i} {num}%{i}={num%i}\t");
+                    if (num % i == 0)
+                    {
+                        is_prime = false;
+                    }
+                }
+                if (num <= 1) is_prime=false;
+                if (num == 2) is_prime=true;
+                if (is_prime)
+                {
+                    Console.WriteLine($"ПРОСТОЕ ЧИСЛО: {num}");
+                    res += num;
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint5.Task5.V17.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KomarovNA.Sprint5.Task5.V17.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
