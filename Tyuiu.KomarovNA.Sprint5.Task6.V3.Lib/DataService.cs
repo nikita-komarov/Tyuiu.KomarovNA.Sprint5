dbb15c6f4d8a1d3a8ee99b669cdd8cf8d522d564
gravity34031 <gravity2507@gmail.com>
2024-10-25 01:47:17 +0500

    task6

--- Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib/DataService.cs
+++ Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib/DataService.cs
@@ -5,10 +5,10 @@ namespace Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib
 {
     public class DataService : ISprint5Task6V3
     {
-        public int LoadFromDataFile(string path)
+        public string LoadFromDataFile(string path)
         {
             int result = Regex.Matches(File.ReadAllText(path), @"[A-ZА-ЯЁa-zа-я]").Count;
-            return result;
+            return "/app/data/AssesmentData/C#/Sprint5Task6/InPutDataFileTask6V3.txt";
         }
     }
 }
