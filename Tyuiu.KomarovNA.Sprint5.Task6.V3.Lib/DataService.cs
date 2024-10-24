d0bb8896f6566c51f06aa7884411a8e69ebcdd68
gravity34031 <gravity2507@gmail.com>
2024-10-25 01:45:56 +0500

    task6

---
+++ Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib
+{
+    public class DataService : ISprint5Task6V3
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int result = Regex.Matches(File.ReadAllText(path), @"[A-ZА-ЯЁa-zа-я]").Count;
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint5.Task6.V3/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KomarovNA.Sprint5.Task6.V3.Lib;
+namespace Tyuiu.KomarovNA.Sprint5.Task6.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #3                                                            *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*                                                                         *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.LoadFromDataFile("C:\\Users\\gravi\\Desktop\\projects\\asylum\\c# sprints\\Tyuiu.KomarovNA.Sprint5\\data\\InPutDataFileTask6V3.txt");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
